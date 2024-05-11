ce4affe801936b2fe96ac3c70f99bd2ef5fef985
MarceloVA <100235694+Marcelo-Dev1624@users.noreply.github.com>
2024-05-11 12:49:08 -0600

    update on menu btn
    Now the menu button hides 3 seconds after no interaction is detected, whether with mouse or touch
    
    except if the menu is opened, the button will not get hidden so the user always knows the menu is opened

--- js/hideModeToggle.js
+++ js/hideModeToggle.js
@@ -1,31 +1,43 @@
 // Get the button element
-const darkLightModeContainer = document.getElementById('darkLightModeContainer');
-const navToggler = document.getElementById('navToggler');
+const navToggler = document.getElementById("navToggler");
 
 // Add an event listener for the 'mousemove' event
 // When the button detects some mouse move on top, set its opacity to 1
 navToggler.addEventListener("mousemove", () => {
-    darkLightModeBlock.style.opacity = "1";
+  navToggler.style.opacity = "1";
+
+
+  // Add an event listener for the 'mouseleave' event
+// When the mouse leaves the button, wait for 3 seconds and then set its opacity to 0.07
+  navToggler.addEventListener("mouseleave", () => {
+    if (navToggler.classList.contains("opened")) {
+      navToggler.style.opacity = "1";
+    } else {
+      setTimeout(() => {
+        navToggler.style.opacity = "0.07";
+      }, 3000);
+    }
+  });
 });
 
 // Add an event listener for the 'touchstart' event
 // When the button is touched, set its opacity to 1
 navToggler.addEventListener("touchstart", () => {
-    darkLightModeBlock.style.opacity = "1";
+
+    // Add an event listener for the 'touchend' event
+// When the touch on the button ends, wait for 3 seconds and then set its opacity to 0.07
+  navToggler.addEventListener("touchend", () => {
+    if (navToggler.classList.contains("opened")) {
+      navToggler.style.opacity = "1";
+    } else {
+      setTimeout(() => {
+        navToggler.style.opacity = "0.07";
+      }, 3000);
+    }
+  });
+  navToggler.style.opacity = "1";
 });
 
-// Add an event listener for the 'mouseleave' event
-// When the mouse leaves the button, wait for 3 seconds and then set its opacity to 0.03
-navToggler.addEventListener("mouseleave", () => {
-    setTimeout(() => {
-        darkLightModeBlock.style.opacity = "0.03";
-    }, 3000);
-});
 
-// Add an event listener for the 'touchend' event
-// When the touch on the button ends, wait for 3 seconds and then set its opacity to 0.03
-navToggler.addEventListener("touchend", () => {
-    setTimeout(() => {
-        darkLightModeBlock.style.opacity = "0.03";
-    }, 3000);
-});
+
+
